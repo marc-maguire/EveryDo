a359bb11df2082d72efbf25d803bf403303cfc61
Marc Maguire <maguire.marc@gmail.com>
2017-05-16 11:10:07 -0400

    Initial Commit

---
+++ EveryDo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  EveryDo
+//
+//  Created by Marc Maguire on 2017-05-16.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ EveryDo/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  EveryDo
+//
+//  Created by Marc Maguire on 2017-05-16.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
